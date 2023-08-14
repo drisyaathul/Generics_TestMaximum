ff861beaa442e6a37607284c254ee7bc3a6d20b5
drisyaathul <drisyak809@gmail.com>
2023-08-15 00:29:36 +0800

    Added: Find Maximum of Three Integers

--- src/com/generics/TestMaximum.java
+++ src/com/generics/TestMaximum.java
@@ -2,4 +2,24 @@ package com.generics;
 
 public class TestMaximum {
 
+    public static void maximumOfThreeIntegers(Integer x, Integer y, Integer z) {
+
+        Integer maximum = x;
+        if (y.compareTo(maximum) > 0)
+            maximum = y;
+        if (z.compareTo(maximum) > 0)
+            maximum = z;
+        System.out.println(maximum+ " is the Maximum of Three Integers");
+    }
+
+    public static void main(String[] args) {
+        //TC1.1:
+        maximumOfThreeIntegers(60,30,50);
+        //TC1.2
+        maximumOfThreeIntegers(50,80,30);
+        //TC1.3
+        maximumOfThreeIntegers(50,60,90);
+    }
+
+
 }
