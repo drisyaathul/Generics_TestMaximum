5f442f37bdb2058837fac4e8a350c34aa4089d46
drisyaathul <drisyak809@gmail.com>
2023-08-15 00:37:01 +0800

    Added: Find Maximum of Three Floats

--- src/com/generics/TestMaximum.java
+++ src/com/generics/TestMaximum.java
@@ -12,13 +12,32 @@ public class TestMaximum {
         System.out.println(maximum+ " is the Maximum of Three Integers");
     }
 
+    public static void maximumOfThreeFloats(Float a, Float b, Float c) {
+
+        Float maximum = a;
+        if (b.compareTo(maximum) > 0)
+            maximum = b;
+        if (c.compareTo(maximum) > 0)
+            maximum = c;
+        System.out.println(maximum+ " is the Maximum of Three Floats");
+    }
+
     public static void main(String[] args) {
+        //Maximum of Three Integers :
         //TC1.1:
         maximumOfThreeIntegers(60,30,50);
         //TC1.2
         maximumOfThreeIntegers(50,80,30);
         //TC1.3
         maximumOfThreeIntegers(50,60,90);
+        System.out.println("-------------------------------------");
+        //Maximum of Three Floats :
+        //TC2.1:
+        maximumOfThreeFloats(25.23f,18.23f,15.79f);
+        //TC2.2
+        maximumOfThreeFloats(25.23f,32.23f,15.79f);
+        //TC2.3
+        maximumOfThreeFloats(25.23f,18.23f,38.21f);
     }
 
 
