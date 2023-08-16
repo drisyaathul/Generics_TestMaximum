88adeefa518d4e686d711c6c943ec429812ea129
drisyaathul <drisyak809@gmail.com>
2023-08-17 01:10:46 +0800

    Added: Take more than Three Parameters

--- src/com/generics/TestMaximum.java
+++ src/com/generics/TestMaximum.java
@@ -1,5 +1,8 @@
 package com.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMaximum <T extends  Comparable<T>> {
 
     T x,y,z;
@@ -10,21 +13,24 @@ public class TestMaximum <T extends  Comparable<T>> {
         this.z = z;
     }
 
-    public static <T extends Comparable <T>> T maximumOfThreeVariables (T x, T y, T z) {
+    public static <T extends Comparable <T>> T maximumOfThreeVariables (T... x) {
 
-        T maximum = x;
-        if (y.compareTo(maximum) > 0)
-            maximum = y;
-        if (z.compareTo(maximum) > 0)
-            maximum = z;
+        T maximum = x[0];
+        List<T> variableArray = new ArrayList<>();
+        for (T element : x) {
+            variableArray.add(element);
+            if (element.compareTo(maximum) > 0) {
+                maximum = element;
+            }
+        }
         return maximum;
     }
 
     public static void main(String[] args) {
 
         System.out.println("*** Maximum Of Three Variables ***");
-        System.out.println("Maximum of three Integers : "+maximumOfThreeVariables(25,20,18));
-        System.out.println("Maximum of three Floats : "+maximumOfThreeVariables(15.5f,30.25f,12.69f));
-        System.out.println("Maximum of three Strings : "+maximumOfThreeVariables("Orange","Peach","Apple"));
+        System.out.println("Maximum of three Integers : "+maximumOfThreeVariables(25,20,18,55,12));
+        System.out.println("Maximum of three Floats : "+maximumOfThreeVariables(15.5f,30.25f,12.69f,32.89f,21.78f));
+        System.out.println("Maximum of three Strings : "+maximumOfThreeVariables("Orange","Peach","Apple","Grape","Plum"));
     }
 }
